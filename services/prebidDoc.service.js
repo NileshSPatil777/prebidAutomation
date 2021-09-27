8b172be4f45c5eb3569040e07fe5ecb5dbd9eb20
isha-kothari <ishak@cybage.com>
2021-09-27 11:38:18 +0530

    Fixed Bid param for some dp and code fix for paramtype

--- services/prebidDoc.service.js
+++ services/prebidDoc.service.js
@@ -32,53 +32,61 @@ function getPrebidDocInfo($, dpDetails) {
   dpDetails.gvlIdDocVal = gvlIdDocVal;
   dpDetails.mediaTypesDocVal = mediaTypesDocVal;
 
-  if ($('table:contains("Scope")')) {
-    var nameIndex, scopeIndex, typeIndex;
-    $('table:contains("Scope")').find('thead').each(function () {
-      $(this).find('tr').find('th').each(function (headingIndex, headingElement) {
-        if ($(this).text().match(/Name/g)) { nameIndex = headingIndex; }
-
-        if ($(this).text().match(/Key/g)) { nameIndex = headingIndex; }
-
-        if ($(this).text().match(/Scope/g)) { scopeIndex = headingIndex; }
-
-        if ($(this).text().match(/Type/g)) { typeIndex = headingIndex; }
-      })
-    })
-    var paramObjArr = [];
-    $('table:contains("Scope")').find('tbody').find('tr').each(function (i, elem) {
-      var paramObj = {};
-      $(this).find('td').each(function (index, element) {
-        var params = $(element).text();
-        if (index == nameIndex) {
-          paramObj.paramName = commonService.changeParamName(params);
-        } else if (index == scopeIndex) {
-          paramObj.required = commonService.chooseRequired(params);
-        } else if (index == typeIndex) {
-          if(typeof(commonService.chooseParamType(params)) == 'string'){
-            delete paramObj.subType;
-            paramObj.paramType = commonService.chooseParamType(params);
-          }else if(typeof(commonService.chooseParamType(params)) == 'object'){
-            paramObj.paramType = commonService.chooseParamType(params).type;
-            paramObj.subType = commonService.chooseParamType(params).subType;
-          }else if(typeof(commonService.chooseParamType(params)) == 'number'){
-            delete paramObj.subType;
-            paramObj.paramType = "Check Manually";
-          } 
-        }
-      })
-      if(paramObj.paramType == undefined){
-        paramObj.paramType = "Check Manually";
-      }
-      var found = paramObjArr.some((el) => {
-        if(el.paramName === paramObj.paramName){
-          el.paramType = "OBJECT"
-         }
-        return el.paramName === paramObj.paramName;
-  });
-  if (!found) { paramObjArr.push(paramObj); }
-    });
+  var paramObjArr = [];
+  
+  if($('p').find('code:contains("string")')){
+    paramObjArr.push({paramName:"Check Manually", required:"Check Manually", paramType:"Check Manually"});
   }
+
+  else{
+    if ($('table:contains("Scope")')) {
+      var nameIndex, scopeIndex, typeIndex;
+      $('table:contains("Scope")').find('thead').each(function () {
+        $(this).find('tr').find('th').each(function (headingIndex, headingElement) {
+          if ($(this).text().match(/Name/g)) { nameIndex = headingIndex; }
+
+          if ($(this).text().match(/Key/g)) { nameIndex = headingIndex; }
+
+          if ($(this).text().match(/Scope/g)) { scopeIndex = headingIndex; }
+
+          if ($(this).text().match(/Type/g)) { typeIndex = headingIndex; }
+        })
+      })
+
+      $('table:contains("Scope")').find('tbody').find('tr').each(function (i, elem) {
+        var paramObj = {};
+        $(this).find('td').each(function (index, element) {
+          var params = $(element).text();
+          if (index == nameIndex) {
+            paramObj.paramName = commonService.changeParamName(params);
+          } else if (index == scopeIndex) {
+            paramObj.required = commonService.chooseRequired(params);
+          } else if (index == typeIndex) {
+            if (typeof (commonService.chooseParamType(params)) == 'string') {
+              delete paramObj.subType;
+              paramObj.paramType = commonService.chooseParamType(params);
+            } else if (typeof (commonService.chooseParamType(params)) == 'object') {
+              paramObj.paramType = commonService.chooseParamType(params).type;
+              paramObj.subType = commonService.chooseParamType(params).subType;
+            } else if (typeof (commonService.chooseParamType(params)) == 'number') {
+              delete paramObj.subType;
+              paramObj.paramType = "Check Manually";
+            }
+          }
+        })
+        if (paramObj.paramType == undefined) {
+          paramObj.paramType = "Check Manually";
+        }
+        var found = paramObjArr.some((el) => {
+          if (el.paramName === paramObj.paramName) {
+            el.paramType = "OBJECT"
+          }
+          return el.paramName === paramObj.paramName;
+        });
+        if (!found) { paramObjArr.push(paramObj); }
+      });
+    }
+}
   dpDetails.bidParamObj = paramObjArr;  
   dpDetails.BidParams = JSON.stringify(paramObjArr,null,4);
   return dpDetails;
